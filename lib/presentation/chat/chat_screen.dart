577f6b8f58c3bc8a5dd36169955fa2ec3a611e45
Xanul <rivas.060@gmail.com>
2023-06-11 10:29:06 -0600

    Added _ChatView widget to show chat messages

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -15,8 +15,32 @@ class ChatScreen extends StatelessWidget {
             ),
           ),
         ),
-        title: const Text("Chat de usuario 😎"),
-        
+        title: const Text("Chat de usuario 💻"),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                return Text("Indice: $index");
+              },) 
+            ),
+            const Text("Hola"),
+          ],
+        ),
       ),
     );
   }
