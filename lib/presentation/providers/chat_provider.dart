55fd900d34ab85280d16440d20ba5f53d99a80eb
Xanul <rivas.060@gmail.com>
2023-06-19 20:26:25 -0600

    Added dio to handle HTTP requests and created /config/helpers/get_yes_no_answert to process HTTP

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import '../../domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
 
   final ScrollController chatScrollController = ScrollController();
-
+  final GetYesNoAnswer getYesNoAnsewer = GetYesNoAnswer();
   
 
   List<Message> messageList = [
@@ -12,15 +13,28 @@ class ChatProvider extends ChangeNotifier {
     Message(text: "Que estas haciendo", fromWho: FromWho.mine)
   ];
 
+  Future<void> otherReply() async {
+    final otherMessage = await getYesNoAnsewer.getAnswer();
+  }
+
   Future<void> sendMessage(String text) async {
+
+    if (text.isEmpty) return;
+
     final newMessage = Message(text: text, fromWho: FromWho.mine);
     messageList.add(newMessage);
+    
+    if (text.endsWith('? ')){
+      otherReply();
+    }    
 
     notifyListeners();
     moveScrollToBottom();
   }
 
-  void moveScrollToBottom() {
+  Future<void> moveScrollToBottom() async {
+
+    await Future.delayed(const Duration(milliseconds: 100));
 
     chatScrollController.animateTo(
       chatScrollController.position.maxScrollExtent + 50, 
